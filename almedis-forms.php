a70684cadd54f20328596aeb4054d8c569977273
Robert Ochoa <ochoa.robert1@gmail.com>
2021-11-02 16:20:11 -0400

    Header Plugin

---
+++ almedis-forms.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Almedis Forms
+ *
+ * @package           AlmedisForms
+ * @author            IndexArt
+ * @copyright         2021 IndexArt
+ * @license           GPL-2.0-or-later
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Almedis Forms
+ * Plugin URI:        https://indexart.cl
+ * Description:       Forms
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            IndexArt
+ * Author URI:        https://example.com
+ * Text Domain:       almedis-forms
+ * License:           GPL v2 or later
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Update URI:        https://example.com/my-plugin/
+ */
+
+
